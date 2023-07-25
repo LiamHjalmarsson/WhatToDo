e2cb61c11cab23209a4ef534e1f158c1abdeaae5
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2023-07-25 19:42:52 +0200

    Deleteing loader item

--- frontend/src/components/Todo/TodoItem.jsx
+++ frontend/src/components/Todo/TodoItem.jsx
@@ -1,31 +1,31 @@
 import { useState } from "react";
 import Button from "../UI/Button/Button";
-import styles from "./TodoItem.module.css";
 import useHttp from "../../hooks/use-http";
 import ItemDescription from "./ItemDecription";
+import styles from "./TodoItem.module.css";
 
-const TodoItem = ({ item, doneHandler, removeHandler, updateTodoHandler }) => { 
+const TodoItem = ({ item, doneHandler, removeHandler, updateTodoHandler }) => {
     let [isEditing, setIsEditing] = useState(false);  // Initializing state variable 'isEditing' and its setter function using the 'useState' hook
     let [editDescription, setEditDescription] = useState(item.description ? item.description : "");  // Initializing state variable 'editDescription' and its setter function with the description of the item or an empty string if there's no description
-    let [title, setTitle] = useState(item.title);  
+    let [title, setTitle] = useState(item.title);
+    let [deleteLoad, setDeleteLoad] = useState(false);
 
-    let { isLoading, sendRequest } = useHttp(); 
+    let { isLoading, sendRequest } = useHttp();
 
-    let toggleEditHandler = () => {  
+    let toggleEditHandler = () => {
         setIsEditing(prevIsEditing => !prevIsEditing);  // Using the previous state to set the new value
     }
 
-    let submitHandler = async () => {  
-
+    let submitHandler = async () => {
         sendRequest(
             {
-                url: `http://127.0.0.1:8000/api/todo-items/${item.id}`, 
+                url: `http://127.0.0.1:8000/api/todo-items/${item.id}`,
                 method: "PUT",
-                headers: { "Content-Type": "application/json" },  
+                headers: { "Content-Type": "application/json" },
                 body: {
-                        title,
-                        description: editDescription 
-                    }  // The new description value to be updated
+                    title,
+                    description: editDescription
+                }  // The new description value to be updated
             },
             (recourse) => {
                 updateTodoHandler(recourse);  // Callback function to handle the response data from the HTTP request
@@ -34,11 +34,11 @@ const TodoItem = ({ item, doneHandler, removeHandler, updateTodoHandler }) => {
         toggleEditHandler();  // Toggling the 'isEditing' state to exit the editing mode
     };
 
-    let changeEditHandler = (e) => {  
+    let changeEditHandler = (e) => {
         setEditDescription(e.target.value);
     }
 
-    let changeTitleHandler = (e) => {  
+    let changeTitleHandler = (e) => {
         setTitle(e.target.value);
     }
 
@@ -46,17 +46,20 @@ const TodoItem = ({ item, doneHandler, removeHandler, updateTodoHandler }) => {
     if (isLoading) {
         message = "Loading...";
     } 
+    else if (deleteLoad) {
+        message = "Deleting";
+    }
     else {
         message = item.description ? item.description : "No description is available";
     }
 
     return (
-        <li key={item.id} className={`${styles.li} ${styles.li} ${item.completed ? styles.done: undefined} `}> 
+        <li key={item.id} className={`${styles.li} ${styles.li} ${item.completed ? styles.done : undefined} `}>
             {
-                !isEditing && <h4 className={styles.title}>{item.title}</h4>  
+                !isEditing && <h4 className={styles.title}>{item.title}</h4>
             }
 
-            {isEditing && (  
+            {isEditing && (
                 <ItemDescription
                     value={editDescription}  // Passing the current edited description as a value to 'ItemDescription'
                     onChange={changeEditHandler}  // Passing the 'changeEditHandler' function to handle changes in the edited description
@@ -69,35 +72,41 @@ const TodoItem = ({ item, doneHandler, removeHandler, updateTodoHandler }) => {
 
             <p className={styles.p}> {message} </p>
 
-                    <div className={styles.btnContainer}> 
-                        <Button
-                            customClass={styles.button}  
-                            button={{
-                                onClick: () => {
-                                    removeHandler(item.id);
+            {
+                !deleteLoad && (
+                        <div className={styles.btnContainer}>
+                            <Button
+                                customClass={styles.button}
+                                button={{
+                                    onClick: () => {
+                                        setDeleteLoad(true);
+                                        removeHandler(item.id);
+                                    }
+                                }}
+                            >
+                                Remove
+                            </Button>
+                            <Button
+                                customClass={styles.button}
+                                button={{ onClick: toggleEditHandler }}
+                            >
+                                Edit
+                            </Button>
+                            <Button
+                                customClass={styles.button}
+                                button={{
+                                    onClick: () => {
+                                        doneHandler(item)
+                                    }
+                                }}
+                            >
+                                {
+                                    !item.completed ? "Done" : "Not Completed"
                                 }
-                            }}
-                        >
-                            Remove
-                        </Button>
-                        <Button 
-                            customClass={styles.button}  
-                            button={{ onClick: toggleEditHandler }} 
-                        >
-                            Edit
-                        </Button>
-                        <Button
-                            customClass={styles.button} 
-                            button={{
-                                onClick: () => doneHandler(item)
-                            }}
-                        >
-                            {
-                                !item.completed ? "Done" : "Not Completed"
-                            }
-                        </Button>
-                    </div>
-
+                            </Button>
+                        </div>
+                    )
+                }
         </li>
     );
 };
